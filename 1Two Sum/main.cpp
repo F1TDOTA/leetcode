61e92d765b4d572ade8f7e07dddee0452b6e6ab3
zhoujun <849408087@qq.com>
2019-05-01 16:25:17 +0800

    添加第一题 简单

---
+++ 1Two Sum/main.cpp
@@ -0,0 +1,18 @@
+#include <STDIO.H>
+
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+
+
+}
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
